0b6e512d7d2176d72492cc9747507127c7e46204
Chris Adams <cn10cra@gmailcom>
2026-05-11 12:15:27 +0100

    post app books form

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from lib.database_connection import DatabaseConnection
 from lib.book_repository import BookRepository
+from lib.book import Book
 from lib.film_repository import FilmRepository
 from data.authors import authors
 from data.quotes import quotes
 
 # instantiate a Flask app object
 app = Flask(__name__)
+
+
+connection = DatabaseConnection()
+connection.connect()
 
 # Declares a route that listens for a GET request to the path /hello
 # and a method to execute when that request comes in
@@ -17,16 +22,20 @@ def index():
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    connection = DatabaseConnection()
-    connection.connect("booksnfilms")
     book_repository = BookRepository(connection)
     books = book_repository.all()
     return render_template("books.html", books = books)
 
+@app.route('/books', methods=['POST'])
+def create_book():
+    book_repository = BookRepository(connection)
+    book_details = request.form
+    book = Book(None,book_details['title'], book_details['author_name'], book_details['blurb'], book_details['url'])
+    book_repository.create(book)
+    return redirect('/books')
+
 @app.route('/films', methods=['GET'])
 def get_films():
-    connection = DatabaseConnection()
-    connection.connect("booksnfilms")
     film_repository = FilmRepository(connection)
     films = film_repository.all()
     return render_template("films.html", films = films)
