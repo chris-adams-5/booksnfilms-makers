53524803601990b0f28ea794daf8f8869390b23f
Chris Adams <cn10cra@gmailcom>
2026-05-12 17:53:35 +0100

    film repository

--- lib/film_repository.py
+++ lib/film_repository.py
@@ -1,3 +1,5 @@
+from lib.film import Film
+
 
 class FilmRepository:
     def __init__(self, connection):
@@ -7,7 +9,16 @@ class FilmRepository:
         rows = self._connection.execute('SELECT * FROM films')
         films = []
         for film in rows:
-            film_i = f"Title: {film['title']} --- Runtime: {film['run_time_minutes']} --- director:{film['director']} --- imdb: {film['imdb']}"
+            film_i = Film(film['id'],film['title'], film['run_time_minutes'], film['director'],film['imdb'])
             films.append(film_i)
             
         return films
+    
+    def create(self,new_film : Film):
+
+        self._connection.execute('' \
+        'INSERT INTO films (title, director, run_time_minutes, imdb) VALUES (%s, %s, %s, %s)', 
+        [new_film.title, new_film.director, new_film.run_time_mins, new_film.imdb]
+        )
+
+        return None
