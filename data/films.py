c5d8923d65ddbf0fbe98b8ecd2a607a1f4678908
Chris Adams <cn10cra@gmailcom>
2026-05-12 17:53:17 +0100

    films data

---
+++ data/films.py
@@ -0,0 +1,20 @@
+films = [
+    {
+    "title":'Jurassic Park', 
+    "run_time_minutes": 122, 
+    "director": 'Steven Spielberg', 
+    "imdb":'https://www.imdb.com/title/tt0107290/'
+    },
+    {
+    "title":'Snakes on a Plane', 
+    "run_time_minutes": 105, 
+    "director": 'David R Ellis', 
+    "imdb":'https://www.imdb.com/title/tt0107290/'
+    },
+    {
+    "title":'Snakes on a Train', 
+    "run_time_minutes": 91, 
+    "director": 'Peter Mervis', 
+    "imdb":'https://www.imdb.com/title/tt0843873/'
+    },
+]
