a85ca3d958c0f9e3279b6047bfc1da10fd735dbc
Chris Adams <cn10cra@gmailcom>
2026-05-14 10:46:35 +0100

    rename tests booksnfilms to orm

--- tests/test_orm/test_user_repository.py
+++ tests/test_orm/test_user_repository.py
@@ -17,6 +17,72 @@ def test_user_repository_all(db_connection):
         User(2,'speckled_jim','meeeh')
     ]
 
+"""
+Create an entry for a user
+"""
+
+def test_user_repository_create(db_connection):
+    db_connection.seed("seeds/booksnfilms.pgsql")
+    repository = UserRepository(db_connection)
+    new_user = User(None, 'Ford Prefect', 'Mostly_Harml3ss')
+    result = repository.create(new_user)
+    assert result == True
+    users = repository.all()
+    assert users == [
+        User(1,'user', 'password'),
+        User(2,'speckled_jim','meeeh'),
+        User(3, 'Ford Prefect', 'Mostly_Harml3ss')
+    ]
+
+
+"""
+rejects a user if the username is already in the db
+"""
+
+def test_user_repository_create_duplicate(db_connection):
+    db_connection.seed("seeds/booksnfilms.pgsql")
+    repository = UserRepository(db_connection)
+    new_user = User(None, 'Ford Prefect', 'Mostly_Harml3ss')
+    repository.create(new_user)
+    result = repository.create(new_user)
+    assert result == False
+    users = repository.all()
+    assert users == [
+        User(1,'user', 'password'),
+        User(2,'speckled_jim','meeeh'),
+        User(3, 'Ford Prefect', 'Mostly_Harml3ss')
+    ]
+
+"""
+Delete an entry from users
+"""
+
+def test_user_repository_delete(db_connection):
+    db_connection.seed("seeds/booksnfilms.pgsql")
+    repository = UserRepository(db_connection)
+    result = repository.delete(1) # I apologise to no one
+    assert result == None
+    users = repository.all()
+
+    assert users == [
+        User(2,'speckled_jim','meeeh')
+    ]
+    # I put it back in as this was the last test to run and it was causing me trouble
+    db_connection.seed("seeds/booksnfilms.pgsql")
+
+
+"""
+login
+"""
+
+def test_user_login(db_connection):
+    db_connection.seed("seeds/booksnfilms.pgsql")
+    repository = UserRepository(db_connection)
+    new_user = User(None, 'Ford Prefect', 'Mostly_Harml3ss')
+    repository.create(new_user)
+    is_logged_in = repository.login(new_user)
+    assert is_logged_in == True
+
 
 """
 When I call find on UserRepository with an id
@@ -29,37 +95,4 @@ def test_user_repository_find(db_connection):
     user = repository.find(2)
     assert user == User(2,'speckled_jim','meeeh')
 
-"""
-Create an entry for a user
-"""
 
-def test_book_repository_create(db_connection):
-    db_connection.seed("seeds/booksnfilms.pgsql")
-    repository = UserRepository(db_connection)
-    new_user = User(None, 'Ford Prefect', 'Mostly_Harml3ss')
-    result = repository.create(new_user)
-    assert result == None
-    users = repository.all()
-
-    assert users == [
-        User(1,'user', 'password'),
-        User(2,'speckled_jim','meeeh'),
-        User(3, 'Ford Prefect', 'Mostly_Harml3ss')
-    ]
-
-"""
-Delete an entry from users
-"""
-
-def test_book_repository_delete(db_connection):
-    db_connection.seed("seeds/booksnfilms.pgsql")
-    repository = UserRepository(db_connection)
-    result = repository.delete(1) # I apologise to no one
-    assert result == None
-    users = repository.all()
-
-    assert users == [
-        User(2,'speckled_jim','meeeh')
-    ]
-    # I put it back in as this was the last test to run and it was causing me trouble
-    db_connection.seed("seeds/booksnfilms.pgsql")
